45da065d0866a69e03659735c00f09a8a85e0824
이해리 <leehaeri@ihaeliui-MacBookPro.local>
2022-09-28 16:46:45 +0900

    feat(NavBar): mui appbar 작업 임시 주석

--- src/Layout/NavBar/NavBar.tsx
+++ src/Layout/NavBar/NavBar.tsx
@@ -2,16 +2,30 @@ import { useRouter } from "next/router";
 import { BuilderhubLogo } from "../../Components/Logo";
 import { NavBarButton, Wrap } from "./Styles";
 
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
 export const NavBar = () => {
   const router = useRouter();
+
   return (
-    <Wrap>
-      <BuilderhubLogo />
-      <NavBarButton
-        onClick={() => router.push("https://customer.dev.builderhub.io")}
-      >
-        빌더허브 시작하기
-      </NavBarButton>
-    </Wrap>
+    <>
+      {/* <AppBar> */}
+      {/* <Toolbar> */}
+      <Wrap>
+        <BuilderhubLogo />
+        {/* <Typography variant="h6" component="div">
+              Scroll to elevate App bar
+            </Typography> */}
+        <NavBarButton
+          onClick={() => router.push("https://customer.dev.builderhub.io")}
+        >
+          빌더허브 시작하기
+        </NavBarButton>
+      </Wrap>
+      {/* </Toolbar> */}
+      {/* </AppBar> */}
+    </>
   );
 };
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -3,6 +3,11 @@ import { Footer } from "./Footer/Footer";
 import { styled } from "@mui/material";
 import { ReactNode } from "react";
 
+import * as React from "react";
+import Toolbar from "@mui/material/Toolbar";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
+
 const Body = styled("div")(() => ({
   width: "100%",
   display: "flex",
@@ -14,10 +19,38 @@ const Body = styled("div")(() => ({
 interface ILayoutProps {
   children: ReactNode;
 }
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 export const Layout = (props: ILayoutProps) => {
   return (
     <>
+      {/* <HideOnScroll {...props}> */}
       <NavBar />
+      {/* </HideOnScroll> */}
+      {/* <Toolbar /> */}
       <Body>{props.children}</Body>
       <Footer />
     </>
