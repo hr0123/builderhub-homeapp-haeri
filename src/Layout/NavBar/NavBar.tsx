ffd6edd36ac6b4da80f6a1d03b1b688f864ccd90
이해리 <leehaeri@ihaeliui-MacBookPro.local>
2022-09-28 17:56:44 +0900

    feat(NabBar): made HideOnScroll

--- src/Layout/NavBar/NavBar.tsx
+++ src/Layout/NavBar/NavBar.tsx
@@ -5,27 +5,56 @@ import { NavBarButton, Wrap } from "./Styles";
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
 
-export const NavBar = () => {
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+export const NavBar = (props: Props) => {
   const router = useRouter();
 
   return (
     <>
-      {/* <AppBar> */}
-      {/* <Toolbar> */}
-      <Wrap>
-        <BuilderhubLogo />
-        {/* <Typography variant="h6" component="div">
-              Scroll to elevate App bar
-            </Typography> */}
-        <NavBarButton
-          onClick={() => router.push("https://customer.dev.builderhub.io")}
-        >
-          빌더허브 시작하기
-        </NavBarButton>
-      </Wrap>
-      {/* </Toolbar> */}
-      {/* </AppBar> */}
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Wrap>
+              <BuilderhubLogo />
+              <NavBarButton
+                onClick={() =>
+                  router.push("https://customer.dev.builderhub.io")
+                }
+              >
+                빌더허브 시작하기
+              </NavBarButton>
+            </Wrap>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
     </>
   );
 };
